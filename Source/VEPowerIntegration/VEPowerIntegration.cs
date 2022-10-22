8641effd9d31bf69c0164fbac7523be2c0c684d9
Jan witczak <Jan_Witczak@outlook.com>
2022-10-22 22:13:51 +0200

    Migration to VS Code, and restructuring.

--- Source/VEPowerIntegration/VEPowerIntegration.cs
+++ Source/VEPowerIntegration/VEPowerIntegration.cs
@@ -1,6 +1,5 @@
 ﻿using HarmonyLib;
 using VanillaPowerExpanded;
-using GasNetwork;
 using System.Reflection;
 using Verse;
 
@@ -14,20 +13,6 @@ namespace PowerController
 			Harmony harmony = new Harmony("Azuraal.PowerController.VEPowerIntegration");
 			Assembly assembly = Assembly.GetExecutingAssembly();
 			harmony.PatchAll(assembly);
-		}
-	}
-
-	[HarmonyPatch(typeof(CompGasPowerPlant), "CompTick")]
-	class GasPowerPlantPatch
-	{
-		static void Postfix(ref CompGasPowerPlant __instance)
-		{
-			CompPowerController Controller = __instance.parent.GetComp<CompPowerController>();
-			if (Controller != null && __instance.PowerOutput != 0)
-			{
-				__instance.PowerOutput = (-__instance.Props.basePowerConsumption) * Controller.Throttle;
-				__instance.parent.GetComp<CompGasTrader>().GasConsumption = __instance.parent.GetComp<CompGasTrader>().Props.gasConsumption * Controller.Throttle;
-			}
 		}
 	}
 
