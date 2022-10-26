65d0b1eaa59650245f424d778356ea07348c9c31
Jan witczak <Jan_Witczak@outlook.com>
2022-10-26 15:46:22 +0200

    Updated VE Integrations

---
+++ Source/VEHelixienIntegration/VEHelixienIntegration.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using PipeSystem;
+using System.Reflection;
+using Verse;
+
+namespace PowerController
+{
+	[StaticConstructorOnStartup]
+	static class VEPowerHarmonyPatches
+	{
+		static VEPowerHarmonyPatches()
+		{
+			Harmony harmony = new Harmony("Azuraal.PowerController.VEPowerIntegration");
+			Assembly assembly = Assembly.GetExecutingAssembly();
+			harmony.PatchAll(assembly);
+		}
+	}
+
+	[HarmonyPatch(typeof(CompResourceTrader), "get_Consumption")]
+	class NuclearPowerPlantPatch
+	{
+		static void Postfix(ref float __result, ref CompResourceTrader __instance)
+		{
+			CompPowerController Controller = __instance.parent.GetComp<CompPowerController>();
+			if (Controller != null)
+			{
+				__result *= Controller.Throttle;
+			}
+		}
+	}
+	
+}
